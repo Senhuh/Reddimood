cf53a51c2688bf5a340db1431933060191f30b94
zuszen <ma.suzchen@gmail.com>
2025-02-25 15:57:01 +0800

    Frontend
    Adjusted the design to make it look more like the prototype design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,62 +14,60 @@ function App() {
   const [neuCon, setNeuConfidence] = useState(0);
   const [negCon, setNegConfidence] = useState(0);
 
-  // trial just to check the bar
-  setTimeout(() => {
-    setPosConfidence(50);
-    setNeuConfidence(25);
-    setNegConfidence(30);
-  }, 1500);
-
   return (
     <div className="app-container">
       
       <div className="left-column">
         <div>
-          <h1>Reddit Sentiment Analyzer</h1>
-          <div>
+          <h2 className="text-header">Reddit Sentiment Analyzer</h2>
+          <div  className="left-top-cont">
             <input
               type="text"
               placeholder="Reddit Link Post..."
-              className="input-field"
+              className="input-text"
             />
-            <button>
+            <button className="button">
               SUBMIT
             </button>
           </div>
         </div>
         
-        <textarea
-          readOnly
-          className="text-view"
-        />
+          <textarea
+            readOnly
+            className="text-view"
+            placeholder="Analyzed text..."
+          />
       </div>
 
       <div className="right-column">
-        <h1>AI GENERATED PERCENTAGE: </h1>
         <div>
-
-          <div>
-            <img src={posImg} alt="Positve"/>
-              <div className="confidence-bar">
-                <div className="confidence-fill pos" style={{ width: `${posCon}%` }}></div>
-              </div>
+          <div className="bot-container">
+            <img className="confidence-image" src={posImg} alt="Positve"/>
+            <div className="confidence-bar">
+              <div className="confidence-fill pos" style={{ width: `${posCon}%` }}></div>
+            </div>
           </div>
 
-          <div>
-            <img src={neuImg} alt="Neutral"/>
+          <div className="bot-container">
+            <img className="confidence-image" src={neuImg} alt="Neutral"/>
             <div className="confidence-bar">
               <div className="confidence-fill neu" style={{ width: `${neuCon}%` }}></div>
             </div>
           </div>
 
-          <div>
-            <img src={negImg} alt="Negative"/>  
+          <div className="bot-container">
+            <img className="confidence-image" src={negImg} alt="Negative"/>  
             <div className="confidence-bar">
               <div className="confidence-fill neg" style={{ width: `${negCon}%` }}></div>
             </div>
           </div>
+        </div>
 
+        <div className="ai-text-container">
+            <h3 className="ai-text">AI GENERATED PERCENTAGE: </h3>
+            <textarea
+              className="ai-percentage"
+            />
         </div>
       </div>
     </div>
