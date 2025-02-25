5812f73b624021038a8b452ef5f156912d5ec784
zuszen <ma.suzchen@gmail.com>
2025-02-25 12:14:04 +0800

    Draft frontend layout with confidence bars and input field
    Created basic layout with two columns (65/35 split).
    Added input field, textview, button, and images.
    Created confidence bars for confidence scores.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,79 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+
+// local images
+import posImg from "./assets/positive.png";
+import neuImg from "./assets/neutral.png";
+import negImg from "./assets/negative.png";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [postLink, setPostLink] = useState("");
+
+  // State to store confidence scores
+  const [posCon, setPosConfidence] = useState(0);
+  const [neuCon, setNeuConfidence] = useState(0);
+  const [negCon, setNegConfidence] = useState(0);
+
+  // trial just to check the bar
+  setTimeout(() => {
+    setPosConfidence(50);
+    setNeuConfidence(25);
+    setNegConfidence(30);
+  }, 1500);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="app-container">
+      
+      <div className="left-column">
+        <div>
+          <h1>Reddit Sentiment Analyzer</h1>
+          <div>
+            <input
+              type="text"
+              placeholder="Reddit Link Post..."
+              className="input-field"
+            />
+            <button>
+              SUBMIT
+            </button>
+          </div>
+        </div>
+        
+        <textarea
+          readOnly
+          className="text-view"
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="right-column">
+        <h1>AI GENERATED PERCENTAGE: </h1>
+        <div>
+
+          <div>
+            <img src={posImg} alt="Positve"/>
+              <div className="confidence-bar">
+                <div className="confidence-fill pos" style={{ width: `${posCon}%` }}></div>
+              </div>
+          </div>
+
+          <div>
+            <img src={neuImg} alt="Neutral"/>
+            <div className="confidence-bar">
+              <div className="confidence-fill neu" style={{ width: `${neuCon}%` }}></div>
+            </div>
+          </div>
+
+          <div>
+            <img src={negImg} alt="Negative"/>  
+            <div className="confidence-bar">
+              <div className="confidence-fill neg" style={{ width: `${negCon}%` }}></div>
+            </div>
+          </div>
+
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
